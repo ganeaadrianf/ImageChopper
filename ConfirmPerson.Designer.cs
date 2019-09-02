470556f09eb8d60b6649d42116ce697a8e98932f
Adrian Ganea <adrian.ganea@gmail.com>
2019-09-02 23:34:51 +0300

    Add project files.

---
+++ ConfirmPerson.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ImageChopper
+{
+    partial class ConfirmPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbPersoana = new System.Windows.Forms.ComboBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblError = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbPersoana
+            // 
+            this.cmbPersoana.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbPersoana.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbPersoana.FormattingEnabled = true;
+            this.cmbPersoana.Location = new System.Drawing.Point(18, 12);
+            this.cmbPersoana.Name = "cmbPersoana";
+            this.cmbPersoana.Size = new System.Drawing.Size(329, 21);
+            this.cmbPersoana.TabIndex = 0;
+            this.cmbPersoana.KeyUp += new System.Windows.Forms.KeyEventHandler(this.CmbPersoana_KeyUp);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.splitContainer1.Panel1.Controls.Add(this.lblError);
+            this.splitContainer1.Panel1.Controls.Add(this.cmbPersoana);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(850, 450);
+            this.splitContainer1.SplitterDistance = 47;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(365, 12);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 399);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ConfirmPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "ConfirmPerson";
+            this.Text = "Selectati o persoana din lista";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ConfirmPerson_FormClosed);
+            this.Load += new System.EventHandler(this.ConfirmPerson_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.ComboBox cmbPersoana;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ ConfirmPerson.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageChopper
+{
+    public partial class ConfirmPerson : Form
+    {
+        public ConfirmPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmPerson_Load(object sender, EventArgs e)
+        {
+            frmMain.people.Sort();
+            cmbPersoana.DataSource = frmMain.people;
+            cmbPersoana.Text = frmMain.currentPersonText == string.Empty ? cmbPersoana.Text : frmMain.currentPersonText;
+        }
+
+        //private const int CP_NOCLOSE_BUTTON = 0x200;
+        //protected override CreateParams CreateParams
+        //{
+        //    get
+        //    {
+        //        CreateParams myCp = base.CreateParams;
+        //        myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+        //        return myCp;
+        //    }
+        //}
+
+        private void CmbPersoana_KeyUp(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyCode.ToString());
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (cmbPersoana.Text == string.Empty)
+                {
+                    lblError.Text = "Trebuie sa alegeti o persoana!";
+                    return;
+                }
+                if (frmMain.people.Where(p => p == cmbPersoana.Text).Count() == 0)
+                {
+                    frmMain.people.Add(cmbPersoana.Text);
+
+                }
+                frmMain.currentImage.Person = cmbPersoana.Text;
+                frmMain.currentPersonText = cmbPersoana.Text;
+                this.DialogResult = DialogResult.OK;
+                //this.Close();
+            }
+
+        }
+
+        private void ConfirmPerson_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (this.DialogResult != DialogResult.OK)
+                this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
